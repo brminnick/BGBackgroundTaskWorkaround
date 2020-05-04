5c6cd946d3c3f29ad9337a1210299542790a904c
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-05-04 12:56:12 -0700

    FIrst Commit

---
+++ BGBackgroundTaskWorkaround/AppDelegate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace BGBackgroundTaskWorkaround
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : UIApplicationDelegate
+    {
+        public override UIWindow? Window { get; set; }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            UIApplication.SharedApplication.SetMinimumBackgroundFetchInterval(UIApplication.BackgroundFetchIntervalMinimum);
+            return true;
+        }
+
+        public override async void PerformFetch(UIApplication application, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            var alertController = new UIAlertController
+            {
+                Title = "Background Fetch Triggered",
+                Message = "It's working!",
+            };
+
+            var currentViewController = Xamarin.Essentials.Platform.GetCurrentUIViewController();
+
+            await currentViewController.PresentViewControllerAsync(alertController, true);
+
+            completionHandler(UIBackgroundFetchResult.NoData);
+        }
+    }
+}
+
