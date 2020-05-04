e853c76e914f4805e83dd39b69a60ccdc308a012
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-05-04 13:30:51 -0700

    Update AppDelegate.cs

--- BGBackgroundTaskWorkaround/AppDelegate.cs
+++ BGBackgroundTaskWorkaround/AppDelegate.cs
@@ -5,30 +5,30 @@ using UIKit;
 namespace BGBackgroundTaskWorkaround
 {
     [Register(nameof(AppDelegate))]
-    public class AppDelegate : UIApplicationDelegate
+public class AppDelegate : UIApplicationDelegate
+{
+    public override UIWindow? Window { get; set; }
+
+    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
     {
-        public override UIWindow? Window { get; set; }
-
-        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
-        {
-            UIApplication.SharedApplication.SetMinimumBackgroundFetchInterval(UIApplication.BackgroundFetchIntervalMinimum);
-            return true;
-        }
-
-        public override async void PerformFetch(UIApplication application, Action<UIBackgroundFetchResult> completionHandler)
-        {
-            var alertController = new UIAlertController
-            {
-                Title = "Background Fetch Triggered",
-                Message = "It's working!",
-            };
-
-            var currentViewController = Xamarin.Essentials.Platform.GetCurrentUIViewController();
-
-            await currentViewController.PresentViewControllerAsync(alertController, true);
-
-            completionHandler(UIBackgroundFetchResult.NoData);
-        }
+        UIApplication.SharedApplication.SetMinimumBackgroundFetchInterval(UIApplication.BackgroundFetchIntervalMinimum);
+        return true;
     }
+
+    public override async void PerformFetch(UIApplication application, Action<UIBackgroundFetchResult> completionHandler)
+    {
+        var alertController = new UIAlertController
+        {
+            Title = "Background Fetch Triggered",
+            Message = "It's working!",
+        };
+
+        var currentViewController = Xamarin.Essentials.Platform.GetCurrentUIViewController();
+
+        await currentViewController.PresentViewControllerAsync(alertController, true);
+
+        completionHandler(UIBackgroundFetchResult.NoData);
+    }
+}
 }
 
